0c041b5599764aee240941aa634816281af25d03
Temirlan-Br <barakov.temirlan@gmail.com>
2023-11-14 17:34:19 +0600

    Add search function,and categories

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const API_KEY = 'a643ea95-c5dd-49db-a12a-70fac5c2afc9';
-const API_URL_PREMIERES =
-  'https://kinopoiskapiunofficial.tech/api/v2.2/films/premieres?year=2022&month=NOVEMBER';
+const API_PREMIERES =
+  'https://kinopoiskapiunofficial.tech/api/v2.2/films/premieres?year=2023&month=NOVEMBER';
+const API_EXPECTED =
+  'https://kinopoiskapiunofficial.tech/api/v2.2/films/collections?type=CLOSES_RELEASES&page=1';
+const API_POPULAR =
+  'https://kinopoiskapiunofficial.tech/api/v2.2/films/collections?type=TOP_POPULAR_MOVIES&page=1';
+const API_RELEASES =
+  'https://kinopoiskapiunofficial.tech/api/v2.1/films/releases?year=2023&month=NOVEMBER&page=1';
+const API_SEARCH =
+  'https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=';
 
 const form = document.querySelector('form');
 const searchInput = document.querySelector('#search');
@@ -10,7 +18,7 @@ const best = document.querySelector('.best');
 const releases = document.querySelector('.releases');
 const favorites = document.querySelector('.favorites');
 
-getMovies(API_URL_PREMIERES);
+getMovies(API_PREMIERES);
 
 async function getMovies(url) {
   try {
@@ -22,6 +30,7 @@ async function getMovies(url) {
       },
     });
     const respData = await resp.json();
+    console.log(respData);
     showMovies(respData);
   } catch (error) {
     console.log(error);
@@ -30,8 +39,11 @@ async function getMovies(url) {
 
 function showMovies(data) {
   const movies = document.querySelector('.movies');
+  movies.innerHTML = '';
 
-  data.items.forEach((movieItem) => {
+  let filmsArray = data.films || data.results || data.items || data.releases;
+
+  filmsArray.forEach((movieItem) => {
     const movie = document.createElement('div');
     movie.classList.add('movieItem');
     movie.innerHTML = `
@@ -45,9 +57,42 @@ function showMovies(data) {
               (genre) => ` ${genre.genre}`
             )}</div>
             <div class="movie__year">${movieItem.year}</div>
-            <div class="movie__average">6.6</div>
+            ${
+              movieItem.rating &&
+              `<div class="movie__average">${movieItem.rating}</div>`
+            }
           </div>
     `;
     movies.appendChild(movie);
   });
 }
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const apiSearch = `${API_SEARCH}${searchInput.value}`;
+  if (searchInput.value) {
+    getMovies(apiSearch);
+    searchInput.value = '';
+  }
+});
+
+premieres.addEventListener('click', (e) => {
+  e.preventDefault();
+  getMovies(API_PREMIERES);
+});
+
+expected.addEventListener('click', (e) => {
+  e.preventDefault();
+  getMovies(API_EXPECTED);
+});
+
+best.addEventListener('click', (e) => {
+  e.preventDefault();
+  getMovies(API_POPULAR);
+});
+
+releases.addEventListener('click', (e) => {
+  e.preventDefault();
+  getMovies(API_RELEASES);
+});
